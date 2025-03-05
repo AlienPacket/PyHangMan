4f5712ba4d5a3c0fcc23b50015a97ea289abc8fc
AlienPacket <199102679+AlienPacket@users.noreply.github.com>
2025-03-05 12:30:32 +0100

    Start to build the idea. Working on take the word to guess.

--- main.py
+++ main.py
@@ -71,4 +71,15 @@ r"""
 ]
 
 
+def take_word(mode: str) -> str:
+  if mode.lower() == 'p':
+    with open("./wordlist/pokemon.txt") as pokemon_file:
+      list_of_pokemon = pokemon_file.readlines()
+      #Learn randomness to going on with the project
+
+
+
+attempts = 0
+mode = input("Select a mode:\nType 'P' for pokemon mode")
+str_to_guess = take_word(mode)
 
