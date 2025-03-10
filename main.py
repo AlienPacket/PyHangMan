df656cdb01d8d743b137f5424ac06fa19cf5c061
AlienPacket <199102679+AlienPacket@users.noreply.github.com>
2025-03-10 14:54:14 +0100

    Add a python file to keep the art separated from the logic. Do small adjustments about the logic and a custom error handling for the input mode.

--- main.py
+++ main.py
@@ -1,85 +1,37 @@
-PICS = [
-r"""
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-""",
+from random import choice
 
-r"""
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-""",
-
-r"""
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-""",
-
-r"""
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========
-""",
-
-r"""
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-""",
-
-r"""
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-""",
-
-r"""
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-"""
-]
+def take_mode():
+  mode = input("Select a mode:\nType 'P' for pokemon mode\nType 'G' for ghibli mode\nType 'C' for custom mode: ").lower()
+  if mode == 'p' or mode == 'g' or mode == 'c':
+    return (True, mode)
+  else:
+    return (False, "Incorrect type mode. Try again.")
 
 
-def take_word(mode: str) -> str:
-  if mode.lower() == 'p':
+def take_word(mode: str):
+  if mode == 'p':
     with open("./wordlist/pokemon.txt") as pokemon_file:
       list_of_pokemon = pokemon_file.readlines()
-      #Learn randomness to going on with the project
+      str_to_guess = choice(list_of_pokemon).lower()
+
+    return str_to_guess
+
+    
+
+def main():
+
+  
+  attempts = 0
+  mode = take_mode()
+
+  #Continue to show the prompt  if user enter a wrong input
+  while not mode[0]:
+    print(mode[1])
+    mode = take_mode()
+  print("Loading mode...")
+    
+  string_to_guess = take_word(mode[1])
+  print(string_to_guess)
 
 
-
-attempts = 0
-mode = input("Select a mode:\nType 'P' for pokemon mode")
-str_to_guess = take_word(mode)
-
+main()
