6f84623e262a22eb484f1507418fec0fececd83b
AlienPacket <199102679+AlienPacket@users.noreply.github.com>
2025-03-12 23:34:57 +0100

    Add a function to take a letter from the user and to handle possible errors. Adjust the pokemon list.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import choice
+from ascii_art import ART
 
 def take_mode():
   mode = input("Select a mode:\nType 'P' for pokemon mode\nType 'G' for ghibli mode\nType 'C' for custom mode: ").lower()
@@ -12,26 +13,38 @@ def take_word(mode: str):
   if mode == 'p':
     with open("./wordlist/pokemon.txt") as pokemon_file:
       list_of_pokemon = pokemon_file.readlines()
-      str_to_guess = choice(list_of_pokemon).lower()
+      str_to_guess = choice(list_of_pokemon).lower().strip()
 
     return str_to_guess
 
-    
+#Continue to prompt if there is a wrong input(word or not alphabetic character)
+def take_char():
+  char = input("Insert a letter: ")
+  while not char.isalpha() or len(char) > 1:
+    char = input("You have to insert a letter[a-z]: ")
+  return char.lower()
+
 
 def main():
-
-  
   attempts = 0
   mode = take_mode()
 
   #Continue to show the prompt  if user enter a wrong input
   while not mode[0]:
-    print(mode[1])
+    print(mode[1]+ '\n')
     mode = take_mode()
-  print("Loading mode...")
+  print("Loading mode...\n\n")
     
   string_to_guess = take_word(mode[1])
-  print(string_to_guess)
+  dashed_str = len(string_to_guess)*'-'
+
+#Game loop. Later will become a function
+  while attempts < 7 or dashed_str != string_to_guess:
+    print(ART[attempts]+ '\n')
+    print(dashed_str)
+    char = take_char()
 
 
+    
+  
 main()
